f9174ade4c9b006cf0e893e1447733302b22281f
Eric Schles <ericschles@gmail.com>
2015-11-20 11:38:53 -0500

    updating dataframe

--- dataframe.rb
+++ dataframe.rb
@@ -4,47 +4,33 @@ class DataFrame
   def initialize(data:[], headers: [],view:"column")
     @data = data
     @headers = headers
-    @index_mapping = []
+    @index_mapping = {}
     @frame = [] # a list of lists
-    
+    #the index_mapping is going to keep an internal set of references from the 
+    #data to the headers.  the way things should look:
+    #Headers: ["Column A","Column B","Column C"]
+    #Data:        1           2          3
+    #             2           3          4
+    #             1           5          2
+    #The way internally I'll know what value goes in each column is via the index_mapping
+    #The reason for this mapping is because the data will be stored internally as a list, but
+    #brought in as a hash - so data will appear as follows: {"Column A" => 1, "Column B" => 2, "Column 3" => 3}
+    #despite being used stored internally as rows - [ ["Column A","Column B","Column C"] , [1,2,3] ]
+    #
   end
   
   def generate_frame(data,headers: [])
-    @data = data
-    @headers = headers
-    alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
-    if headers != []
-      for header,ind in @headers.each_with_index
-        @frame[header] = @data[ind]
-      end
-    else 
-      for elem in 0..data.length
-        headers.push(alphabet[elem])
-      end
-      for header,ind in @headers.each_with_index
-        @frame[header] = @data[ind]
-      end
-    end
+   
   end
 
   def to_s
-    if @view == "column"
-      print @headers.join(' ')
-      puts "Column by Column view"
-      for column in @data
-        puts column.join(',')
-      end
    
-    elsif @view == "row"
-      print @headers.join(' ')
-      
-    end
   end
 end
 
 
 if __FILE__ == $0
   df = DataFrame.new
-  df.generate_frame([["Hello there","Hi"],["Ahoy friends","what up"]],headers: ["greetings","replies"])
+  df.generate_frame()
   puts df
 end
