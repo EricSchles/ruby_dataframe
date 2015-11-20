c881e938e6089808176f6a6778d741e2fd30905e
Eric Schles <ericschles@gmail.com>
2015-11-20 10:56:51 -0500

    updating dataframe

--- dataframe.rb
+++ dataframe.rb
@@ -1,6 +1,6 @@
 #expects ruby 2.1.2
 class DataFrame
- 
+  attr_accessor :headers,:data,:frame
   def initialize(data:[], headers: [])
     @data = data
     @headers = headers
@@ -13,6 +13,8 @@ class DataFrame
   end
   
   def generate_frame(data,headers: [])
+    @data = data
+    @headers = headers
     alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
     if headers != []
       for header,ind in @headers.each_with_index
@@ -28,8 +30,23 @@ class DataFrame
     end
   end
 
+  def to_s()
+    print @headers.join(' ')
+    for column in @data
+      puts column
+    end
+    
+  end
+  
 end
+
 
 if __FILE__ == $0
   df = DataFrame.new
+  df.generate_frame([["Hello there","Hi"],["Ahoy friends","what up"]],headers: ["greetings","replies"])
+  puts "headers"
+  puts df.headers
+  puts "data"
+  puts df.data
   
+end
